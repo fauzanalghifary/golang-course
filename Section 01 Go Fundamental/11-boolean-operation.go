d148ec42fa1bfeaaf9e7647cc0974179a306b152
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-03-04 07:45:55 +0700

    continue section 01

---
+++ Section 01 Go Fundamental/11-boolean-operation.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Boolean Operation
+	// &&, ||, !
+	// &&: AND
+	// ||: OR
+	// !: NOT
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
---
+++ Section 01 Go Fundamental/9-math-operation.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	result := 1 + 2
+	println(result)
+
+	result = 1 - 2
+	println(result)
+
+	result = 1 * 2
+	println(result)
+
+	result = 1 / 2
+	println(result)
+
+	result = 1 % 2
+	println(result)
+
+	result += 10
+	println(result)
+
+	result++
+	println(result)
+}
