e63f0037a37a91e1bd74db4dfa975fce07e94686
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-03-11 23:35:05 +0700

    continue section 01

--- Section 01 Go Fundamental/35-interface.go
+++ Section 01 Go Fundamental/35-interface.go
@@ -1,1 +1,35 @@
 package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	var person Person = Person{"Rizky"}
+	SayHello(person)
+
+	cat := Animal{"Kucing"}
+	SayHello(cat)
+}
